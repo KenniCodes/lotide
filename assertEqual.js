01f0c7344d3fc111e72009fc2b720c757a68ade0
KenniCodes <kennedygosnell@outlook.com>
2023-09-18 22:43:20 -0700

    Assertion testing a function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🫡 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🥺 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("String", "string");
+assertEqual(3, 19);
