cda706cb3d200c8ab9809b53cddde4f0f51e0ccb
KenniCodes <kennedygosnell@outlook.com>
2023-09-25 13:24:35 -0700

    DRY middle + created middleTest

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`🫡 Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🥺 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(arr) {
   const length = arr.length;
   if (length < 3) {
@@ -29,9 +9,4 @@ const middle = function(arr) {
     return [arr[length / 2 -1], arr[length / 2]];
   }
 };
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2 , 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
